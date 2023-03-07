55deae3a7c498f86a4bf0fed7fbe4c2f1026d037
Артур Шакиров <Wolverane32@gmail.com>
2023-03-07 11:32:33 +0500

    add folder for custom scripts

---
+++ wwwroot/js/CustomValidation/customValidation.js
@@ -0,0 +1,13 @@
+﻿var uploadField = document.getElementById("avatar");
+
+uploadField.onchange = function () {
+    if (this.files[0].size > 2097152) {
+        let errorsFiled = document.getElementById("Input_avatar-error");
+        errorsFiled.innerText = "Размер изображения не должен превышать 2 мегабайта";
+
+        this.value = "";
+    }
+    else {
+        $("#Input_avatar-error").text("");
+    };
+};
