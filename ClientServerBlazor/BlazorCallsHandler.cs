bdefeb1149da35ee87ec0f21b8f1def80a11b754
Артур Шакиров <Wolverane32@gmail.com>
2023-04-02 16:42:23 +0500

    moder tools

--- ClientServerBlazor/BlazorCallsHandler.cs
+++ ClientServerBlazor/BlazorCallsHandler.cs
@@ -6,12 +6,19 @@ namespace CourseWorkSpring2023.ClientServerBlazor
     public class BlazorCallsHandler
     {
 
-        public BlazorCallsHandler(FollowersRepository followersRepository)
+        public BlazorCallsHandler(FollowersRepository followersRepository, 
+            PostsRepository postsManager, ContentRepository contentManager, CommentsRepository commentsManager)
         {
             _followersRepository = followersRepository;
+            this.postsManager = postsManager;
+            this.contentManager = contentManager;
+            this.commentsManager = commentsManager;
         }
 
-        public FollowersRepository _followersRepository;
+        private FollowersRepository _followersRepository;
+        private PostsRepository postsManager;
+        private ContentRepository contentManager;
+        private CommentsRepository commentsManager;
 
         public void Follow(string followerId, string userToFollow)
         {
@@ -21,6 +28,26 @@ namespace CourseWorkSpring2023.ClientServerBlazor
         public void UnFollowUser(string followerId, string userToUnFollow)
         {
             _followersRepository.UnFollowUser(followerId, userToUnFollow);
+        }
+
+        public void DeletePost(int postId)
+        {
+            postsManager.Delete(postId);
+        }
+
+        public void DeleteComment(int commentId) 
+        {
+            commentsManager.Delete(commentId);
+        }
+
+        public void Hide(int id)
+        {
+            contentManager.Hide(id);
+        }
+
+        public void Show(int id)
+        {
+            contentManager.UnHide(id);
         }
     }
 }
