d87905b185226b5ad8c827cba68c5ada60b2122a
Артур Шакиров <Wolverane32@gmail.com>
2023-04-05 14:15:15 +0500

    moder fixes

---
+++ Models/IndexViewModel/PostInfoViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace CourseWorkSpring2023.Models.IndexViewModel
+{
+    public class PostInfoViewModel
+    {
+        public PostViewModel Post { get; set; }
+
+        public bool IsModer { get; set; } = false;
+    }
+}
